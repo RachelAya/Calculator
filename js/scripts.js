4b40e81886931ad0dd1cb28bfcc7c847102d6dab
RachelAya <rachel.ayateke@gmail.com>
2018-04-24 14:32:02 +0300

    add code from JSFiddle

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var add = function(number1, number2)
+ {
+    return number1 + number2;
+  };
+var number1= prompt("Enter a number:");
+var number2= prompt("Enter a second one:");
+alert(number1,number2);
+
+  var substract = function(number1, number2)
+  {
+    return number1 - number2;
+  };
+var number1= prompt("Enter a number:");
+var number2= prompt("Enter a second one:");
+alert(number1,number2);
+
+  var multiply = function(number1, number2)
+ {
+    return number1 * number2;
+  };
+var number1= prompt("Enter a number:");
+var number2= prompt("Enter a second one:");
+alert(number1,number2);
+
+  var division = function(number1, number2)
+  {
+    return number1 / number2;
+  };
+var number1= prompt("Enter a number:");
+var number2= prompt("Enter a second one:");
+alert(number1,number2);
