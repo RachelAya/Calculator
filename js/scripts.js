97b34ff99c180698a7ee4b110f0421496659bbf7
RachelAya <rachel.ayateke@gmail.com>
2018-04-26 12:34:40 +0300

    separating business logic and user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,35 @@
-var add = function(number1, number2)
- {
-    return number1 + number2;
-  };
-var number1= prompt("Enter a number:");
-var number2= prompt("Enter a second one:");
-alert(number1,number2);
+var add = function(number1, number2) {
+        return number1 + number2;
+      };
 
-  var substract = function(number1, number2)
-  {
-    return number1 - number2;
-  };
-var number1= prompt("Enter a number:");
-var number2= prompt("Enter a second one:");
-alert(number1,number2);
+      var substract = function(number1, number2) {
+        return number1 - number2;
+      };
 
-  var multiply = function(number1, number2)
- {
-    return number1 * number2;
-  };
-var number1= prompt("Enter a number:");
-var number2= prompt("Enter a second one:");
-alert(number1,number2);
+      var multiply = function(number1, number2) {
+        return number1 * number2;
+      };
 
-  var division = function(number1, number2)
-  {
-    return number1 / number2;
-  };
-var number1= prompt("Enter a number:");
-var number2= prompt("Enter a second one:");
-alert(number1,number2);
+      var divide = function(number1, number2) {
+        return number1 / number2;
+      };
+
+      $(document).ready(function() {
+        $("form#add").submit(function(event) {
+          event.preventDefault();
+          var number1 = parseInt($("#add1").val());
+          var number2 = parseInt($("#add2").val());
+          var result = add(number1, number2);
+        $("#outputAdd").text(result);
+        });
+      });
+
+        $(document).ready(function() {
+        $("form#sub").submit(function(event) {
+          event.preventDefault();
+          var number1 = parseInt($("#sub1").val());
+          var number2 = parseInt($("#sub2").val());
+          var result = substract(number1, number2);
+        $("#outputSub").text(result);
+        });
+      });
